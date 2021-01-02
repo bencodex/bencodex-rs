042857b2d42c58999205e6bdc012bcc680bc574b
moreal <dev.moreal@gmail.com>
2021-01-03 05:00:23 +0900

    feat: re-export common items

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
 pub mod bencodex;
 
 mod tests;
+
+pub use bencodex::codec::decode::{Decode, DecodeError, DecodeErrorReason};
+pub use bencodex::codec::encode::Encode;
+pub use bencodex::codec::types::{BencodexKey, BencodexValue};
