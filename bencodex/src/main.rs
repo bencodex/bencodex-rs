26045437e17ae0e48463826520abe2b73d235ecd
Moreal <dev.moreal@gmail.com>
2024-06-09 15:26:05 +0900

    feat: introduce bencodex CLI

---
+++ bencodex/src/main.rs
@@ -0,0 +1,20 @@
+use bencodex::Decode;
+use std::io::Read;
+
+fn main() {
+    let mut buf = Vec::new();
+    if let Err(err) = std::io::stdin().read_to_end(&mut buf) {
+        eprintln!("{:?}", err);
+        return;
+    }
+
+    let decoded = match buf.decode() {
+        Ok(value) => value,
+        Err(err) => {
+            eprintln!("Failed to decode. {:?}", err);
+            return;
+        }
+    };
+
+    println!("{:?}", decoded);
+}
