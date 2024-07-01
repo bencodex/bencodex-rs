5ace55602e084bbc48981b08b47daf9dcaf4bf4d
Moreal <dev.moreal@gmail.com>
2024-07-01 22:19:50 +0900

    feat: provide a flag to choose a way to encode binary

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
+use bencodex::json::encode::{to_json_with_options, BinaryEncoding, JsonOptions};
 use bencodex::Decode;
+use clap::Parser;
 use std::io::Read;
 
+/// A program to encode and decode between Bencodex and JSON.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Encode Bencodex Binary by base64 string.
+    /// If not given, it will encode as hexadecimal string.
+    #[arg(short, long)]
+    base64: bool,
+}
+
 fn main() {
+    let args = Args::parse();
     let mut buf = Vec::new();
     if let Err(err) = std::io::stdin().read_to_end(&mut buf) {
         eprintln!("{:?}", err);
@@ -16,5 +29,13 @@ fn main() {
         }
     };
 
-    println!("{}", decoded);
+    let json_encode_options = JsonOptions {
+        binary_encoding: if args.base64 {
+            BinaryEncoding::Base64
+        } else {
+            BinaryEncoding::Hex
+        },
+    };
+
+    println!("{}", to_json_with_options(&decoded, json_encode_options));
 }
